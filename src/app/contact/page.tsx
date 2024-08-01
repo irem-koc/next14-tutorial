40b98ef891b9d0670941bd1133a07771526a9ee0
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-01 16:11:54 +0300

    feat: add contact page and module.css

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+import Image from "next/image";
+import styles from "./contact.module.css";
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src="/contact.png" alt="" fill />
+      </div>
+      <form
+        onSubmit={() => console.log("hello")}
+        className={styles.formContainer}
+      >
+        <input required type="text" placeholder="Name and Surname" />
+        <input required type="text" placeholder="Email address" />
+        <input type="text" placeholder="Phone Number (Optional)" />
+        <textarea
+          rows={5}
+          placeholder="Message"
+          required
+          name=""
+          id=""
+        ></textarea>
+        <button className={styles.button} type="submit">
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default ContactPage
+export default ContactPage;
