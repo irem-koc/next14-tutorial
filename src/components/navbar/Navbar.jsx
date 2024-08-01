4a9d31063e60c3eb2ad23471a9467f9395efb735
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-01 13:55:56 +0300

    feat: add navbar and module.css file

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,17 @@
-import Links from './links/Links';
-
+import Link from "next/link";
+import Links from "./links/Links";
+import styles from "./navbar.module.css";
 const Navbar = () => {
   return (
-    <div>
-      <div>Logo</div>
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>
+        Logo
+      </Link>
       <div>
-       <Links />
-
+        <Links />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
