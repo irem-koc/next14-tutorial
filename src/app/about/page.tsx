8fa7b629e64ed127faa3707dd97dadb610acecf0
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-01 13:19:07 +0300

    feat: using external images

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,13 @@
+import Image from "next/image"
+import styles from "./about.module.css"
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div>
+      <div className={styles.imgContainer}>
+      <Image src="https://images.pexels.com/photos/27298696/pexels-photo-27298696/free-photo-of-a-swan-is-standing-on-the-ice-in-the-water.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" fill alt="about" />
+
+      </div>
+    </div>
   )
 }
 
