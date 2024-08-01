fb8caa74ee1bb9d81858f3e8cda43ebbff9cac34
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-01 14:53:30 +0300

    feat: add about page and module.css file

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,39 @@
-import Image from "next/image"
-import styles from "./about.module.css"
+import Image from "next/image";
+import styles from "./about.module.css";
 const AboutPage = () => {
   return (
-    <div>
-      <div className={styles.imgContainer}>
-      <Image src="https://images.pexels.com/photos/27298696/pexels-photo-27298696/free-photo-of-a-swan-is-standing-on-the-ice-in-the-water.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" fill alt="about" />
-
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.about}>About Agency</h2>
+        <h1 className={styles.header}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={styles.subText}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precision We're world's Our
+          Special Team best consulting & finance solution provider. Wide range
+          of web and software development services.
+        </p>
+        <div className={styles.colContainer}>
+          <div className={styles.col}>
+            <h2 className={styles.colHeader}>10 K+</h2>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.col}>
+            <h2 className={styles.colHeader}>234 K+</h2>
+            <p>People reached</p>
+          </div>
+          <div className={styles.col}>
+            <h2 className={styles.colHeader}>5 K+</h2>
+            <p>Services and plugins</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src="/about.png" alt="about" fill />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
