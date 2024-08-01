383944a613c5d1813cda8b04292af33afde771ee
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-01 13:56:52 +0300

    feat: add home page and module.css file

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,32 @@
+import Image from "next/image";
+import styles from "./home.module.css";
 const Home = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos
+          assumenda nostrum modi molestias quas architecto.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact Us</button>
+        </div>
+        <div className={styles.brands}>
+          <Image
+            src={"/brands.png"}
+            alt="brands"
+            fill
+            className={styles.brandImage}
+          />
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src={"/hero.gif"} alt="brands" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
